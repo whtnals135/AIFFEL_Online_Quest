7f54cfdecf57c60cdb40df853340bad1b562b831
Sumin Jo <149548856+whtnals135@users.noreply.github.com>
2024-02-08 17:19:56 +0900

    Update final_result.dart

--- Flutter_Quest/FL_03/lib/final_result.dart
+++ Flutter_Quest/FL_03/lib/final_result.dart
@@ -5,7 +5,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  bool _is_cat = true;
+  bool _is_cat = true; // 고양이인지 여부를 나타내는 변수
 
   @override
   Widget build(BuildContext context) {
@@ -13,10 +13,10 @@ class MyApp extends StatelessWidget {
       title: 'Navigator Test',
       home: Navigator(
         pages: [
-          MaterialPage(child: SecondScreen()),
-          if (_is_cat) MaterialPage(child: FirstScreen())
+          MaterialPage(child: SecondScreen()), // 두 번째 화면을 표시하는 페이지
+          if (_is_cat) MaterialPage(child: FirstScreen()) // 첫 번째 화면을 표시하는 페이지 (조건부)
         ],
-        onPopPage: (route, result) => route.didPop(result),
+        onPopPage: (route, result) => route.didPop(result), // 페이지가 팝될 때의 동작 정의
       ),
     );
   }
@@ -27,9 +27,9 @@ class FirstScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Icon(Icons.pets),
+        leading: Icon(Icons.pets), // 앱바 왼쪽에 표시되는 아이콘
         title: Center(
-          child: Text('First page'),
+          child: Text('First page'), // 앱바 타이틀
         ),
       ),
       body: Column(
@@ -43,12 +43,12 @@ class FirstScreen extends StatelessWidget {
                   onPressed: () {
                     Navigator.push(
                       context,
-                      MaterialPageRoute(builder: (context) => SecondScreen()),
+                      MaterialPageRoute(builder: (context) => SecondScreen()), // 다음 화면으로 이동하는 버튼
                     );
                   },
-                  child: Text('Next'),
+                  child: Text('Next'), // 버튼 텍스트
                 ),
-                SizedBox(height: 20),
+                SizedBox(height: 20), // 버튼과 이미지 사이 공간
                 Align(
                   alignment: Alignment.center,
                   child: Image.asset(
@@ -71,9 +71,9 @@ class SecondScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Icon(Icons.favorite),
+        leading: Icon(Icons.favorite), // 앱바 왼쪽에 표시되는 아이콘
         title: Center( // 중앙 정렬
-        child: Text('Second Page'),
+          child: Text('Second Page'), // 앱바 타이틀
         ),
       ),
       body: Column(
@@ -85,9 +85,9 @@ class SecondScreen extends StatelessWidget {
               children: [
                 ElevatedButton(
                   onPressed: () {
-                    Navigator.pop(context); // 이전 화면으로 돌아가기
+                    Navigator.pop(context); // 이전 화면으로 돌아가는 버튼
                   },
-                  child: Text('Back'),
+                  child: Text('Back'), // 버튼 텍스트
                 ),
                 SizedBox(height: 20), // 버튼과 이미지 사이 공간
                 Align(
